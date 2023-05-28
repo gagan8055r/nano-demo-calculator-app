af68b5348facc7ca756737bf633b0ea5f78239c4
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-05-28 16:03:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+class Result {
+  constructor(result) {
+    this.result = result;
+  }
+}
 
 app.use(express.json());
 
-app.get("/calculator/greeting", (req, res) => {
-  res.status(200).send("Hello world!");
+app.get('/calculator/greeting', (req, res) => {
+  res.send('Hello world!');
 });
 
-app.post("/calculator/add", (req, res) => {
+app.post('/calculator/add', (req, res) => {
   const { first, second } = req.body;
-  const result = first + second;
-  res.status(200).json({ result });
+  const response = new Result(first + second);
+  res.json(response);
 });
 
-app.post("/calculator/subtract", (req, res) => {
+app.post('/calculator/subtract', (req, res) => {
   const { first, second } = req.body;
-  const result = first - second;
-  res.status(200).json({ result });
+  const response = new Result(first - second);
+  res.json(response);
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
